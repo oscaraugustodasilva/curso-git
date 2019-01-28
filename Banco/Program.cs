fa8d4e9c9248487092f215b8fe87e331854022d2
Oscar Augusto <ti3@LEVCRED.LOCAL>
2019-01-28 12:53:54 -0200

    Projeto iniciado, mostrando a mensagem

---
+++ Banco/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Entre com o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Havera deposito inicial (S/N)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 'S' || resp == 's')
+            {
+                Console.WriteLine("Entre com o valor do deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Entre com o valor para deposito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizado: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Entre com o valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizado: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine(DateTime.Now);
+        }
+
+    }
+}
